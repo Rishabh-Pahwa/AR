ba3f85dc921623c67d696db9e459ab339700370b
Rishabh Pahwa <rishabh8may@gmail.com>
2023-07-03 20:36:44 +0530

    Rectile & door are rendered but position is mismat

--- src/App.js
+++ src/App.js
@@ -20,11 +20,15 @@ function App() {
   
   const ViewinAR2D =()=>{
 
+
+      let container;    
       let camera, scene, renderer;
       let controller;
+
+      let reticle_line;
   
-      // let hitTestSource = null;
-     // let hitTestSourceRequested = false;
+      let hitTestSource = null;
+      let hitTestSourceRequested = false;
 
       init();
       animate();
@@ -37,13 +41,13 @@ function App() {
       function init(){
 
 
-        const container = document.createElement( 'div' );
+        container = document.createElement( 'div' );
         document.body.appendChild( container );
       
         scene = new THREE.Scene();
 
-        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100000 );
-        //camera.position.set( 0, 0, 4);
+        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 2000);
+       // camera.position.set( 0, 0, 40);
     
         
         // LIGHTS
@@ -60,17 +64,19 @@ function App() {
         renderer.xr.enabled = true;
         container.appendChild(renderer.domElement);
     
-        document.body.appendChild(ARButton.createButton(renderer));
+        document.body.appendChild( ARButton.createButton( renderer, { requiredFeatures: [ 'hit-test' ] } ) );
     
-
         const geometry = new THREE.PlaneGeometry( 2, 4 );
 
         function onSelect(){
-        const texture = new THREE.TextureLoader().load( './sw_door.png' );
-        texture.colorSpace = THREE.SRGBColorSpace;
-        const material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide } );
-        const mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
+          if(reticle_line.visible){
+            const texture = new THREE.TextureLoader().load( './sw_door.png' );
+            texture.colorSpace = THREE.SRGBColorSpace;
+            const material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide } );
+            const plane = new THREE.Mesh( geometry, material );
+            reticle_line.matrix.decompose( plane.position, plane.quaternion, plane.scale );
+            scene.add( plane );
+          }
         }
     
     
@@ -78,6 +84,34 @@ function App() {
         controller = renderer.xr.getController(0);
         controller.addEventListener( 'select', onSelect );
         scene.add(controller);
+
+
+        // // Create a geometry for the line
+        // const Lgeometry = new THREE.BufferGeometry();
+        // const lineLength = 4; // Length in world units
+        // const vertices = new Float32Array([
+        //   0, 0, 0,           // Start point
+        //   lineLength, 0, 0   // End point
+        // ]);
+        // geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+        // // Create a material for the line
+        // const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+
+        // // Create a line segments object
+        //  reticle_line = new THREE.LineSegments(Lgeometry, material);
+
+        // // Add the line to the scene
+        // scene.add(reticle_line);
+
+        reticle_line = new THREE.Mesh(
+					new THREE.RingGeometry( 0.15, 0.2, 32 ),
+					new THREE.MeshBasicMaterial()
+				);
+				reticle_line.matrixAutoUpdate = false;
+				reticle_line.visible = false;
+				scene.add( reticle_line );
+
 
         window.addEventListener( 'resize', onWindowResize );
 
@@ -94,15 +128,69 @@ function App() {
     
       
   
-      console.log(scene)
+      //console.log(scene)
 
       function animate() {
         renderer.setAnimationLoop(render);
       }
 
-      function render(){
-        renderer.render(scene, camera);
-      }
+      // function render(){
+      //   renderer.render(scene, camera);
+      // }
+      function render( timestamp, frame ) {
+
+				if ( frame ) {
+
+					const referenceSpace = renderer.xr.getReferenceSpace();
+					const session = renderer.xr.getSession();
+
+					if ( hitTestSourceRequested === false ) {
+
+						session.requestReferenceSpace( 'viewer' ).then( function ( referenceSpace ) {
+
+							session.requestHitTestSource( { space: referenceSpace } ).then( function ( source ) {
+
+								hitTestSource = source;
+
+							} );
+
+						} );
+
+						session.addEventListener( 'end', function () {
+
+							hitTestSourceRequested = false;
+							hitTestSource = null;
+
+						} );
+
+						hitTestSourceRequested = true;
+
+					}
+
+					if ( hitTestSource ) {
+
+						const hitTestResults = frame.getHitTestResults( hitTestSource );
+
+						if ( hitTestResults.length ) {
+
+							const hit = hitTestResults[ 0 ];
+
+							reticle_line.visible = true;
+							reticle_line.matrix.fromArray( hit.getPose( referenceSpace ).transform.matrix );
+
+						} else {
+
+							reticle_line.visible = false;
+
+						}
+
+					}
+
+				}
+
+				renderer.render( scene, camera );
+
+			}
       
       
   
@@ -122,7 +210,7 @@ return(
           className="action-button"
           onClick={ViewinAR2D}
         >
-          VIEW (2D Image)
+          View in AR
         </button>
         {/* <div> 
           {ViewinAR2D}
